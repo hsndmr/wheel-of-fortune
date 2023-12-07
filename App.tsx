081a0cb533ca617b44fc6b92bd7215ba40dcde6e
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-08 00:32:26 +0300

    feat: add WheelOfFortune component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { WheelOfFortune } from "./src/components/WheelOfFortune";
 
 export default function App() {
+  const segments = ["🐶", "🐺", "🐱", "🐯", "🦁", "🦄", "🐈", "🦭"];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WheelOfFortune segments={segments} />
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
